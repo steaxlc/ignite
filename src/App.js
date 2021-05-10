f89a6eaaf0ed67d8f1e689a53d47b1c6457124bb
Luciano Castro <contatolucianocastro@gmail.com>
2021-05-09 22:14:25 -0300

    more test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <GlobalStyles />
-      <Route path={['/ignite/game/:id', '/']}>
+      <Route path={['/ignite/game/:id', '/ignite/']}>
         <Home/>
       </Route>
       
